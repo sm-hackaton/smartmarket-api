7095aeef3258631254b6d4d7f7f605be4c161d03
David Iruzubieta <david.iruzubieta@gmail.com>
2016-06-12 03:33:57 +0200

    changed database schema.

--- migrations/20160611131853-add-devices.js
+++ migrations/20160611131853-add-devices.js
@@ -5,6 +5,15 @@ exports.up = function(db, next) {
     db.createTable('devices', {
         id: {type: 'int', primaryKey: true, autoIncrement: true},
         uuid: {type: 'string', unique: true},
+        owner_id: {
+            type: 'int',
+            foreignKey: {
+                name: 'devices_owner_id_fk',
+                mapping: 'id',
+                table: 'accounts',
+                rules: {onUpdate: 'CASCADE', onDelete: 'CASCADE'}
+            }
+        },
         account_id: {
             type: 'int',
             foreignKey: {
